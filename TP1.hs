4348925cece0435e541287018c18288e27b4353f
Ana Sofia <anasofiab.27@gmail.com>
2024-11-03 01:39:45 +0000

    travelSales done

--- TP1.hs
+++ TP1.hs
@@ -12,6 +12,8 @@ type Path = [City]
 type Distance = Int
 
 type RoadMap = [(City,City,Distance)]
+type AdjMatrix = Data.Array.Array (Int, Int) (Maybe Distance)
+
 
 cities :: RoadMap -> [City]
 cities r = Data.List.nub ([o| (o,_,_)<-r] ++ [p|(_,p,_)<-r])
@@ -49,13 +51,21 @@ dfs r v c
 
 bfs :: RoadMap -> [Path] -> [City] -> City -> [Path]
 bfs r [] _ _ = [] 
-bfs r (h:tl) v e
-      | c == e = [h]  
+bfs r (h:tl) v e 
+      | c == e = h : bfs r tl v e 
       | otherwise = bfs r (tl ++ newPaths) (c : v) e
       where
         c = last h
         adj = getAdjacent c r
         newPaths = [h ++ [next] | next <- adj, next `notElem` v]
+
+filterMinPaths :: [Path] -> RoadMap -> [Path]
+filterMinPaths paths r =
+    let distances = [d | Just d <- map (pathDistance r) paths]
+    in case distances of
+        [] -> []
+        _  -> let minDist = minimum distances
+              in [path | path <- paths, pathDistance r path == Just minDist]        
 
 isStronglyConnected :: RoadMap -> Bool
 isStronglyConnected r =
@@ -69,11 +79,78 @@ getAdjacent :: City -> RoadMap -> [City]
 getAdjacent city roads = [if c1 == city then c2 else c1 | (c1, c2, _) <- roads, c1 == city || c2 == city ]
 
 shortestPath :: RoadMap -> City -> City -> [Path]
-shortestPath r s e = bfs r [[s]] [] e
+shortestPath r s e = filterMinPaths (bfs r [[s]] [] e) r
     
 
+buildAdjMatrix :: [City] -> RoadMap -> AdjMatrix
+buildAdjMatrix cities r =
+    Data.Array.array bounds [((i, j), getDistance i j cities r) | i <- [0 .. numCities - 1], j <- [0 .. numCities - 1]]
+  where
+    numCities = length cities
+    bounds = ((0, 0), (numCities - 1, numCities - 1))
+
+
+-- Finds the distance between cities by their indices in the cities list
+
+getDistance :: Int -> Int -> [City] -> RoadMap -> Maybe Distance
+getDistance i j cities r =
+    let cityPairs = [((c1, c2), d) | (c1, c2, d) <- r] ++ [((c2, c1), d) | (c1, c2, d) <- r]
+    in lookup (cities !! i, cities !! j) cityPairs
+
+
 travelSales :: RoadMap -> Path
-travelSales = undefined
+travelSales r
+  | null citiesL = []
+  | otherwise = case bestResult of
+      Nothing -> []
+      Just (_, path) -> map (citiesL !!) (0 : path)
+  where
+    citiesL = cities r
+    numCities = length citiesL
+    adjMatrix = buildAdjMatrix citiesL r
+
+    -- Memoization table for dynamic programming
+    buildMemoTable = Data.Array.array ((0, 0), (2 ^ numCities - 1, numCities - 1)) 
+      [ ((visitedMask, pos), tsp visitedMask pos) 
+      | visitedMask <- [0 .. 2 ^ numCities - 1], pos <- [0 .. numCities - 1]]
+
+    -- Dynamic programming function for Traveling Salesman Problem with bitmasking
+    tsp :: Int -> Int -> Maybe (Distance, [Int])
+    tsp visitedMask pos
+      | allCitiesVisited visitedMask = returnToStart
+      | otherwise = findMinimum validMoves
+      where
+        allCitiesVisited mask = mask == (1 `Data.Bits.shiftL` numCities) - 1
+        returnToStart = case adjMatrix Data.Array.! (pos, 0) of
+                            Just distance -> Just (distance, [0])
+                            Nothing -> Nothing
+
+        validMoves = [ 
+            let followingDist = adjMatrix Data.Array.! (pos, next)
+            in case followingDist of
+                Just d -> do
+                    (existingDist, existingPath) <- buildMemoTable Data.Array.! 
+                        (visitedMask Data.Bits..|. (1 `Data.Bits.shiftL` next), next)
+                    Just (d + existingDist, next : existingPath)
+                Nothing -> Nothing
+            | next <- [0 .. numCities - 1],
+            next /= pos,
+            not (Data.Bits.testBit visitedMask next)
+            ]   
+
+    findMinimum :: [Maybe (Distance, [Int])] -> Maybe (Distance, [Int])
+    findMinimum mvs =
+      let validMvs = [v | Just v <- mvs]
+      in case validMvs of
+          [] -> Nothing
+          _ -> Just $ Data.List.minimumBy comparePaths validMvs        
+
+    comparePaths :: (Distance, [Int]) -> (Distance, [Int]) -> Ordering
+    comparePaths (d1, _) (d2, _) = compare d1 d2
+
+    -- Final result from memoization
+    bestResult = buildMemoTable Data.Array.! (1, 0)
+
 
 tspBruteForce :: RoadMap -> Path
 tspBruteForce = undefined -- only for groups of 3 people; groups of 2 people: do not edit this function
